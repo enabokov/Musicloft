92b6fc9388a4c3df601996337b6800a897588738
Alexander Onbysh <onbysh.a@gmail.com>
2016-09-29 22:38:02 +0300

    Add AlbumView, BandView, BandListView, AlbumListView; Create dynamic links between pages, set url redirection

--- music/urls.py
+++ music/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
-    url(r'^$', views.IndexView, name='index'),
-    url(r'^bands/$', views.BandView.as_view(), name='bands'),
-    url(r'^albums/$', views.AlbumView.as_view(), name='albums'),
+    url(r'^$', views.index_view, name='index'),
+    url(r'^band/(?P<pk>[0-9]+)/$', views.BandView.as_view(), name='band'),
+    url(r'^album/(?P<pk>[0-9]+)/$', views.AlbumView.as_view(), name='album'),
+    url(r'^bands/$', views.BandListView.as_view(), name='bands'),
+    url(r'^albums/$', views.AlbumListView.as_view(), name='albums'),
 ]
