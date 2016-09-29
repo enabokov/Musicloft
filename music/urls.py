914a177decdd3bb6bd8ba81fd59a1c3e0b978bbc
Alexander Onbysh <onbysh.a@gmail.com>
2016-09-29 21:32:12 +0300

    Add IndexView (HomePage), reorganise html code (separate base html)

--- music/urls.py
+++ music/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.IndexView, name='index'),
     url(r'^bands/$', views.BandView.as_view(), name='bands'),
     url(r'^albums/$', views.AlbumView.as_view(), name='albums'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,19 +1,30 @@
+from django.shortcuts import render
 from django.views import generic
 
-from .models import Band, Album
+from .models import Band, Album, Song
 
 
 class BandView(generic.ListView):
+    model = Band
     template_name = 'music/BandView.html'
     context_object_name = 'bands'
 
-    def get_queryset(self):
-        return Band.objects.all()
-
 
 class AlbumView(generic.ListView):
+    model = Album
     template_name = 'music/AlbumView.html'
     context_object_name = 'albums'
 
-    def get_queryset(self):
-        return Album.objects.all()
+
+def IndexView(request):
+    bands = Band.objects.all()
+    albums = Album.objects.all()
+    songs = Song.objects.all()
+
+    context = {
+        'bands': bands,
+        'albums': albums,
+        'songs': songs
+    }
+
+    return render(request, 'music/index.html', context)
