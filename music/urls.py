1e9399f079023c15d032937b4b84e4bad6ebd0a2
Alexander Onbysh <onbysh.a@gmail.com>
2016-10-01 21:15:01 +0300

    Add registration, log in and log out views Add user account page

--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,16 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    url(r'^$', views.index_view, name='index'),
-    url(r'^register/$', views.register, name='register'),
+    url(r'^$', views.index, name='index'),
+    # User urls
+    url(r'^registration/$', views.register, name='register'),
+    url(r'^login/$', views.login_user, name='login'),
+    url(r'^logout/$', views.logout_user, name='logout'),
+    url(r'^account/$', views.user_account, name='account'),
+    # Data urls
     url(r'^bands/$', views.BandListView.as_view(), name='bands'),
     url(r'^albums/$', views.AlbumListView.as_view(), name='albums'),  # All albums
     url(r'^band/(?P<pk>[0-9]+)/$', views.BandView.as_view(), name='band'),
     url(r'^album/(?P<pk>[0-9]+)/$', views.AlbumView.as_view(), name='album'),
-    url(r'^albums/(?P<pk>[0-9]+)/$', views.AlbumListView.as_view(), name='albums'),  # For precise band
+    url(r'^albums/(?P<pk>[0-9]+)/$', views.AlbumListView.as_view(), name='albums'),  # Albums for precise band
 ]
--- music/views.py
+++ music/views.py
@@ -1,65 +1,19 @@
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render
 from django.views import generic
 
-from .forms import UserForm
-from .models import Band, Album, Song
+from .models import Band, Album
 
 
-class BandView(generic.DetailView):
-    model = Band
-    queryset = Band.objects.all()
+class BandView(generic.ListView):
     template_name = 'music/BandView.html'
-    context_object_name = 'band'
-
-
-class AlbumView(generic.DetailView):
-    model = Album
-    queryset = Album.objects.all()
-    template_name = 'music/AlbumView.html'
-    context_object_name = 'album'
-
-
-class BandListView(generic.ListView):
-    model = Band
-    template_name = 'music/BandListView.html'
     context_object_name = 'bands'
 
+    def get_queryset(self):
+        return Band.objects.all()
 
-class AlbumListView(generic.ListView):
-    model = Album
-    template_name = 'music/AlbumListView.html'
+
+class AlbumView(generic.ListView):
+    template_name = 'music/AlbumView.html'
     context_object_name = 'albums'
 
-
-def index_view(request):
-    bands = Band.objects.all()
-    albums = Album.objects.all()
-    songs = Song.objects.all()
-
-    context = {
-        'bands': bands,
-        'albums': albums,
-        'songs': songs
-    }
-
-    return render(request, 'music/index.html', context)
-
-
-def register(request):
-    form = UserForm(request.POST or None)
-    if form.is_valid():
-        user = form.save(commit=False)
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        user.set_password(password)
-        user.save()
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return index_view(request)
-    context = {
-        "form": form,
-    }
-    return render(request, 'music/register.html', context)
+    def get_queryset(self):
+        return Album.objects.all()
